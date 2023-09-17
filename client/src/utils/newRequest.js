f1d52fb911751d1a07d599ed9194a45266f8b78a
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-09-18 04:48:58 +0530

    ADDED URL TO FRONT END

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'https://jobjuggle.onrender.com',
   withCredentials: true,
 });
 

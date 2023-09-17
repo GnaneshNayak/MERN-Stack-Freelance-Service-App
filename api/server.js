299a376a8d11307785bdffc426a7d68be70f3310
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-09-18 03:04:50 +0530

    add cors error

--- api/server.js
+++ api/server.js
@@ -65,7 +65,13 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTENT_LINK, credentials: true }));
+// app.use(cors({ origin: 'https://jobjuggle.netlify.app/', credentials: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your domain
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Enable credentials
+
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);

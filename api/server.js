62019da35c3ca5c31750f7ea8bd7ae37dd3ba362
GnaneshNayak <mr.gnaneshnayak@gmail.com>
2023-09-18 03:18:20 +0530

    Update CORS policy to allow requests from 'https://jobjuggle.netlify.app/' with credentials

--- api/server.js
+++ api/server.js
@@ -66,21 +66,16 @@ const connect = async () => {
 app.use(express.json());
 app.use(cookieParser());
 // app.use(cors({ origin: 'https://jobjuggle.netlify.app/', credentials: true }));
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your domain
+  res.header('Access-Control-Allow-Origin', 'https://jobjuggle.netlify.app');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
   );
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Enable credentials
-
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests (OPTIONS method)
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 
 app.use('/api/auth', authRoute);
